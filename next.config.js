9248adbe8e5b4e87bea1874884b715f380b365c5
Mario Bustillo <mario.bustillo@kroger.com>
2021-03-13 21:47:08 -0600

    css

--- next.config.js
+++ next.config.js
@@ -1,23 +1,21 @@
 const path = require("path");
 const nextEnv = require("next-env");
 const dotenvLoad = require("dotenv-load");
-const withPurgeCss = require("next-purgecss");
+
 const withImages = require("next-images");
 dotenvLoad();
 
 const withNextEnv = nextEnv();
 
-const config = withPurgeCss(
-  withNextEnv(
-    withImages({
-      purgeCssPaths: ["pages/**/*", "components/**/*"],
-      purgeCss: {
-        whitelist: () => ["player"],
-        whitelistPatterns: () => [/Toastify/, /.*nprogress.*/],
-        rejected: true,
-      },
-      purgeCssEnabled: ({ dev, isServer }) => true, // Enable PurgeCSS for all env
-    })
-  )
+module.exports = withNextEnv(
+  withImages({
+    webpack: (config) => {
+      config.resolve.alias["components"] = path.resolve(
+        __dirname,
+        "components"
+      );
+      config.resolve.alias["public"] = path.resolve(__dirname, "public");
+      return config;
+    },
+  })
 );
-module.exports = config;
