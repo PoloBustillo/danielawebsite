5bdde415cdbeaf7b5aeec4d8a170bcfd9dd326a5
PoloBustillo <leopoldobeguiluz1@hotmail.com>
2022-07-01 14:39:47 -0500

    auth 2

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,36 +8,45 @@ const options = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
-  session: {
-    jwt: true,
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout",
+    error: "/auth/error", // Error code passed in query string as ?error=
+    verifyRequest: "/auth/verify-request", // (used for check email message)
+    newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
   },
-  database: process.env.NEXT_PUBLIC_DATABASE_URL,
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    session: async (session, user) => {
-      console.log("SESION", sesion);
-      console.log("USER", user);
-      session.jwt = user.jwt;
-      session.id = user.id;
-      return Promise.resolve(session);
-    },
-    jwt: async ({ token, user, account }) => {
-      console.log("POLO1", token);
-      console.log("POLO2", user);
-      console.log("POLO3", account);
-      const isSignIn = user ? true : false;
-      if (isSignIn) {
-        const response = await fetch(
-          `https://daniela-cms-2.herokuapp.com/auth/google/callback?access_token=${account?.access_token}`
-        );
-        const data = await response.json();
-        console.log("DARTA", data);
-        token.jwt = data.jwt;
-        token.id = data.user.id;
-      }
-
-      return Promise.resolve(token);
-    },
+    // session: async (session, user) => {
+    //   console.log("SESION", sesion);
+    //   console.log("USER", user);
+    //   session.jwt = user.jwt;
+    //   session.id = user.id;
+    //   return Promise.resolve(session);
+    // },
+    // jwt: async ({ token, user, account }) => {
+    //   console.log("POLO1", token);
+    //   console.log("POLO2", user);
+    //   console.log("POLO3", account);
+    //   if (account) {
+    //     token.accessToken = account.access_token;
+    //     return token;
+    //   }
+    //   const isSignIn = user ? true : false;
+    //   if (isSignIn) {
+    //     const response = await fetch(
+    //       `https://daniela-cms-2.herokuapp.com/auth/google/callback?access_token=${account?.access_token}`
+    //     );
+    //     const data = await response.json();
+    //     console.log("DARTA", data);
+    //     token.jwt = data.jwt;
+    //     token.id = data.user.id;
+    //   }
+    //   return Promise.resolve(token);
+    // },
   },
 };
 
