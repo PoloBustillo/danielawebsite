3f08858d2f4bb493ccd56d035ed8348139c56379
PoloBustillo <leopoldobeguiluz1@hotmail.com>
2022-07-01 15:16:39 -0500

    auuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -27,26 +27,21 @@ const options = {
     //   session.id = user.id;
     //   return Promise.resolve(session);
     // },
-    // jwt: async ({ token, user, account }) => {
-    //   console.log("POLO1", token);
-    //   console.log("POLO2", user);
-    //   console.log("POLO3", account);
-    //   if (account) {
-    //     token.accessToken = account.access_token;
-    //     return token;
-    //   }
-    //   const isSignIn = user ? true : false;
-    //   if (isSignIn) {
-    //     const response = await fetch(
-    //       `https://daniela-cms-2.herokuapp.com/auth/google/callback?access_token=${account?.access_token}`
-    //     );
-    //     const data = await response.json();
-    //     console.log("DARTA", data);
-    //     token.jwt = data.jwt;
-    //     token.id = data.user.id;
-    //   }
-    //   return Promise.resolve(token);
-    // },
+    jwt: async ({ token, user, account }) => {
+      console.log("POLO1", token);
+      console.log("POLO2", user);
+      console.log("POLO3", account);
+
+      const isSignIn = user ? true : false;
+      if (isSignIn) {
+        const response = await fetch(
+          `https://daniela-cms-2.herokuapp.com/auth/google/callback?access_token=${account?.access_token}`
+        );
+        const data = await response.json();
+        console.log("DARTA", data);
+      }
+      return token;
+    },
   },
 };
 
